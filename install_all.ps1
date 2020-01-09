dfc936b9669fa048f268558bd3a354ad3b24cad2
prog-dude <sindre.nevland@gmail.com>
2020-01-09 18:23:20 +0100

    add apps

---
+++ install_all.ps1
@@ -0,0 +1,17 @@
+scoop install balabolka
+scoop install bittorrent
+scoop install citra
+scoop install equalizerapo
+scoop install flstudio
+scoop install geforce
+scoop install gog2
+scoop install logos
+scoop install midiox
+scoop install nordvpn
+scoop install office07
+scoop install pc_equalizer_gui
+scoop install pdfcandy
+scoop install razersynapse2
+scoop install scite4ahk
+scoop install visualboyadvance
+#scoop install 
