131d15d8f8c5b679da454dd0e15289a33a6cfd25
prog-dude <sindre.nevland@gmail.com>
2020-01-09 20:21:26 +0100

    add apps

--- install_all.ps1
+++ install_all.ps1
@@ -1,10 +1,12 @@
-scoop install balabolka
-scoop install bittorrent
-scoop install citra
-scoop install equalizerapo
+Write-Host "------------------"
+Write-Host "------------------"
+#scoop install balabolka
+#scoop install bittorrent
+#scoop install citra
+#scoop install equalizerapo
 scoop install flstudio
 scoop install geforce
-scoop install gog2
+#scoop install gog2
 scoop install logos
 scoop install midiox
 scoop install nordvpn
