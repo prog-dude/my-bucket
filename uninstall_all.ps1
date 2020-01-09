90b4703de0a3201eec290af667504fc1c8eee0b3
prog-dude <sindre.nevland@gmail.com>
2020-01-09 19:04:56 +0100

    add apps

---
+++ uninstall_all.ps1
@@ -0,0 +1,16 @@
+scoop uninstall balabolka
+scoop uninstall bittorrent
+scoop uninstall citra
+scoop uninstall equalizerapo
+scoop uninstall flstudio
+scoop uninstall geforce
+scoop uninstall gog2
+scoop uninstall logos
+scoop uninstall midiox
+scoop uninstall nordvpn
+scoop uninstall office07
+scoop uninstall pc_equalizer_gui
+scoop uninstall pdfcandy
+scoop uninstall razersynapse2
+scoop uninstall scite4ahk
+scoop uninstall visualboyadvance
